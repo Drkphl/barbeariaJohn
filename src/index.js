8ba85991425b2961603002e597f1dfbc1ef715e4
leachenski <jleachenski@gmail.com>
2024-08-28 09:55:33 -0300

    feat: add middleware that map cep to address

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const cep_endereco = require("./middlewares/cep_endereco.js")
+const app = express()
+const port = 5000
+
+app.use(express.json())
+// app.use(cep_endereco) // Midleware de uso global
+
+app.post("/barbearia", cep_endereco, (req, res) => {
+    res.json(req.body)
+})
+
+app.listen(port, () => {
+    console.log(`Server running in ${port} port`)
+})
